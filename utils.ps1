4a9154d45160e2d3941fae39927161100e7dca90
GoLab18 <kuba1golab@gmail.com>
2026-01-21 23:19:18 +0100

    Added func for checking diskshadow availability

--- utils.ps1
+++ utils.ps1
@@ -5,13 +5,20 @@
 .DESCRIPTION
     Contains helper functions like Require-Admin which checks for administrator rights
     and exits the script if the current user lacks sufficient privileges.
+    Also contains a helper to test if diskshadow.exe is available.
 #>
 
 function Require-Admin {
-    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
-    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Error "Must be run as administrator!"
+    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $p  = New-Object Security.Principal.WindowsPrincipal($id)
+
+    if (-not $p.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Error "This script must be run as Administrator."
         exit 1
     }
+}
+
+function Test-DiskShadowAvailable {
+    $dsPath = Join-Path $env:WINDIR "System32\diskshadow.exe"
+    return (Test-Path $dsPath)
 }
